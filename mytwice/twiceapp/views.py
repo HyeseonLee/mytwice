5cdbbf7b41656e730b4559fc20c5448cdd716d89
Hyeseon Lee <hyesun1999@gmail.com>
2019-05-23 07:10:53 +0900

    first commit of 6th_work

---
+++ mytwice/twiceapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Twice
+
+# Create your views here.
+def twice(request):
+    members=Twice.objects.filter(nationality='JP')
+    return render(request, 'home.html', {'japanese':members})
